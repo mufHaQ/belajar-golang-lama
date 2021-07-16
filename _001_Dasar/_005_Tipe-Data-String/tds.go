45e327c66346d5cf9c580f442636cffd43a94c2c
mufHaQ <masbrowmess33@gmail.com>
2021-07-16 14:22:31 +0700

    add: _005_Tipe-Data-String

---
+++ _001_Dasar/_005_Tipe-Data-String/tds.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("===Tipe Data String===")
+
+	// fmt.Println("Dliyaulhaq")
+	// fmt.Println("Dliyaulhaq Mufliansyah")
+
+	fmt.Println(len("Dliyaulhaq"))
+	fmt.Println("Dliyaulhaq"[0]) // Akan mengembalikan byte dari 'D'
+}
