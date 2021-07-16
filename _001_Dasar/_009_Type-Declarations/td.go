4cad0e1da6449fbb9fa689d142855289bd26b863
mufHaQ <masbrowmess33@gmail.com>
2021-07-16 18:12:20 +0700

    add: _009_Type-Declarations

---
+++ _001_Dasar/_009_Type-Declarations/td.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("===Type Declarations===")
+
+	// ================================================================================
+	// Type Declarations:
+
+	type Nama string
+	type Kerja bool
+
+	var myName Nama = "Dliyaulhaq"
+	var bekerja Kerja = false
+	fmt.Println(myName)
+	fmt.Println(bekerja)
+	// ================================================================================
+}
