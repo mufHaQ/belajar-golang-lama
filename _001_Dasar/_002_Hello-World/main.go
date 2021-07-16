259e0ca5538e4fdc80aef0234098e6f3d9cc0891
mufHaQ <masbrowmess33@gmail.com>
2021-07-16 12:00:27 +0700

    add: _002_Hello-World

---
+++ _001_Dasar/_002_Hello-World/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() { // Semua function dijalankan di dalam function main()
+	fmt.Println("Hello World")
+}
+
+// Menjalankan tanpa compile: go run nama_file.go
+// Untuk mengcompile golang dengan command: go build nama_file.go
